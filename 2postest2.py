e88e09c79b2de9547e44feebce0d19ddb473ce06
akabanelay <92873925+akabanelay@users.noreply.github.com>
2021-10-21 22:04:23 +0800

    Add files via upload

---
+++ 2postest2.py
@@ -0,0 +1,13 @@
+# program sederhana untuk inputan biodata
+list = ["Dinda Nur Afifah", 'Samarinda', "12 September 2003", 18, 156.7, 48.6, 
+'Mahasiswa', 'UNMUL', 'FT', 'Informatika']
+print ('Nama :', list[0])
+print ("Tempat Lahir :", list[1])
+print ("Taggal Lahir :", list[2])
+print ('Umur :', list[3])
+print ("Tinggi Badan :", list[4], 'Cm')
+print ("Berat Badan :", list[5], 'Kg')
+print ('Status :', list[6])
+print ("Asal Universitas :", list[7])
+print ('Fakultas :', list[8])
+print ('prodi :', list[9])
---
+++ 3postest.py
@@ -0,0 +1,13 @@
+# program sederhana untuk inputan biodata
+dict = {'Nama': "Dinda Nur Afifah",  "Tempat dan tanggal lahir": "Samarinda 12 September 2003", 'Umur': 18, 
+"Tinggi Badan" : 156.7, "Berat Badan": 48.6, 'Status': 'Mahasiswa', "Asal Universitas": 'UNMUL', 
+'Fakultas': 'FT', 'Prodi': 'Informatika'}
+print ('Nama : ', dict['Nama'])
+print ("Tempat dan tanggal lahir : ", dict["Tempat dan tanggal lahir"])
+print ('Umur :', dict['Umur'])
+print ("Tinggi Badan :", dict["Tinggi Badan"], 'Cm')
+print ("Berat Badan :", dict["Berat Badan"], 'Kg')
+print ('Status :', dict['Status'])
+print ("Asal Universitas :", dict["Asal Universitas"])
+print ('Fakultas :', dict['Fakultas'])
+print ('Prodi :', dict['Prodi'])
